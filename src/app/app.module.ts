e9a88af7faa276ace2d055e93d7baff8e6abfabf
thiago <thiagosm.tech@gmail.com>
2023-03-29 17:31:09 -0300

    feat: route configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { effects, reducers } from './store/app.state';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot(effects),
+    StoreDevtoolsModule.instrument(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
